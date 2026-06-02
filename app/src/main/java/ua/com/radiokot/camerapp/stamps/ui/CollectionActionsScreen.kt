db5f9396593d3325c45b20f811215aaf0ea23b05
Oleg Koretsky <oleg@radiokot.com.ua>
2026-06-02 18:12:00 +0300

    Add landscape version for collection actions screen

--- app/src/main/java/ua/com/radiokot/camerapp/stamps/ui/CollectionActionsScreen.kt
+++ app/src/main/java/ua/com/radiokot/camerapp/stamps/ui/CollectionActionsScreen.kt
@@ -19,20 +19,24 @@
 
 package ua.com.radiokot.camerapp.stamps.ui
 
+import android.content.res.Configuration
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -45,6 +49,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -63,15 +68,6 @@ fun CollectionActionsScreen(
     onDeleteAction: () -> Unit,
     sharedTransitionScope: SharedTransitionScope?,
     animatedVisibilityScope: AnimatedVisibilityScope?,
-) = Column(
-    horizontalAlignment = Alignment.CenterHorizontally,
-    verticalArrangement = Arrangement.Center,
-    modifier = modifier
-        .safeContentPadding()
-        .verticalScroll(
-            state = rememberScrollState(),
-            overscrollEffect = null,
-        )
 ) {
     val detailsAlpha = remember {
         Animatable(0f)
@@ -85,6 +81,60 @@ fun CollectionActionsScreen(
         )
     }
 
+    val configuration = LocalConfiguration.current
+    if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier
+                .safeContentPadding()
+        ) {
+            CollectionActionsScreenLayoutContent(
+                detailsAlpha = detailsAlpha,
+                collection = collection,
+                canDelete = canDelete,
+                onMoveStampsAction = onMoveStampsAction,
+                onDeleteAction = onDeleteAction,
+                sharedTransitionScope = sharedTransitionScope,
+                animatedVisibilityScope = animatedVisibilityScope,
+            )
+        }
+    } else {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier
+                .safeContentPadding()
+                .verticalScroll(
+                    state = rememberScrollState(),
+                    overscrollEffect = null,
+                )
+        ) {
+            CollectionActionsScreenLayoutContent(
+                detailsAlpha = detailsAlpha,
+                collection = collection,
+                canDelete = canDelete,
+                onMoveStampsAction = onMoveStampsAction,
+                onDeleteAction = onDeleteAction,
+                sharedTransitionScope = sharedTransitionScope,
+                animatedVisibilityScope = animatedVisibilityScope,
+            )
+
+            Spacer(modifier = Modifier.size(24.dp))
+        }
+    }
+}
+
+@Composable
+private fun CollectionActionsScreenLayoutContent(
+    detailsAlpha: Animatable<Float, AnimationVector1D>,
+    collection: CollectionListItem,
+    canDelete: Boolean,
+    onMoveStampsAction: () -> Unit,
+    onDeleteAction: () -> Unit,
+    sharedTransitionScope: SharedTransitionScope?,
+    animatedVisibilityScope: AnimatedVisibilityScope?,
+) {
     StampBoxView(
         name = collection.name,
         someStamps = collection.someStamps,
@@ -93,7 +143,7 @@ fun CollectionActionsScreen(
         animatedVisibilityScope = animatedVisibilityScope,
     )
 
-    Spacer(modifier = Modifier.height(24.dp))
+    Spacer(modifier = Modifier.size(24.dp))
 
     Actions(
         canDelete = canDelete,
@@ -105,8 +155,6 @@ fun CollectionActionsScreen(
                 alpha = detailsAlpha.value
             }
     )
-
-    Spacer(modifier = Modifier.height(24.dp))
 }
 
 @Composable
